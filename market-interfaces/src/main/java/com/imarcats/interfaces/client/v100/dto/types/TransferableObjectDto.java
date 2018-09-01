9257651afe26b2810d2aef35eac52eb24024ed02
Acer <Acer@DESKTOP-263QF6M>
2018-09-01 22:12:53 +0200

    Making market DTOs ready for Jackson serialization

--- market-interfaces/src/main/java/com/imarcats/interfaces/client/v100/dto/types/TransferableObjectDto.java
+++ market-interfaces/src/main/java/com/imarcats/interfaces/client/v100/dto/types/TransferableObjectDto.java
@@ -1,9 +1,9 @@
 package com.imarcats.interfaces.client.v100.dto.types;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.imarcats.interfaces.client.v100.dto.MarketModelObjectDto;
 
 public interface TransferableObjectDto extends MarketModelObjectDto {
-
+	@JsonIgnore
 	public Object getObjectValue();
-	
 }
